98661024602dc26373d1c8c414d0ff366e93599a
2026-DEV2-017 <>
2026-01-17 17:29:35 +0100

    fix converter to pass when input is 2 minutes

--- BerlinClock/BerlinClock/DigitalTimeToBerlinClockConverter.swift
+++ BerlinClock/BerlinClock/DigitalTimeToBerlinClockConverter.swift
@@ -5,8 +5,10 @@ nonisolated struct DigitalTimeToBerlinClockConverter {
     func singleMinutesRow(minutes: Int) -> String {
         if minutes == 0 {
             return "0000"
-        } else {
+        } else if minutes == 1 {
             return "Y000"
+        } else {
+            return "YY00"
         }
     }
     
