5db162443ead5580e549734c0295524b6122961b
2026-DEV2-017 <>
2026-01-17 17:20:35 +0100

    add extra cases to test using parameters
    - test implementation for 1 minute

--- BerlinClock/BerlinClockTests/DigitalTimeToBerlinClockConverterTests.swift
+++ BerlinClock/BerlinClockTests/DigitalTimeToBerlinClockConverterTests.swift
@@ -6,16 +6,22 @@ struct DigitalTimeToBerlinClockConverterTests {
     @Suite("Single minutes row")
     struct SingleMinutesRowTests {
         
-        @Test("Given an initialised converter, when converting single minutes, then the correct lamps are returned")
-        func singleMinutes() async throws {
+        @Test(
+            "Given an initialised converter, when converting single minutes, then the correct lamps are returned",
+            arguments: [
+                (minutes: 0, expectedResult: "0000"),
+                (minutes: 1, expectedResult: "Y000"),
+            ]
+        )
+        func singleMinutes(minutes: Int, expectedResult: String) async throws {
             // Given
             let converter = DigitalTimeToBerlinClockConverter()
             
             // When
-            let result = converter.singleMinutesRow(minutes: 0)
+            let result = converter.singleMinutesRow(minutes: minutes)
             
             // Then
-            #expect(result == "0000")
+            #expect(result == expectedResult)
         }
         
     }
