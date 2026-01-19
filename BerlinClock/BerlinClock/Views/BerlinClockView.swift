29b017e50ae526c0c8dbb667b99dcd4f371bedc7
2026-DEV2-017 <>
2026-01-19 20:17:52 +0100

    add seconds lamp to view
    - show seconds lamp
    - hook up model to ui
    - start/stop model in `onAppear` and `onDisappear` respectively

---
+++ BerlinClock/BerlinClock/Views/BerlinClockView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct BerlinClockView: View {
+    
+    @State private var model = BerlinClockModel(timeProvider: TimerTimeProvider())
+    
+    var body: some View {
+        VStack {
+            Circle()
+                .fill(model.seconds == "Y" ? Color.yellow : .clear)
+                .strokeBorder(lineWidth: 2)
+        }
+        .padding()
+        .onAppear {
+            model.start()
+        }
+        .onDisappear {
+            model.stop()
+        }
+    }
+}
+
+#Preview {
+    BerlinClockView()
+}
