10b8ffe66b8e88515ff561744a95111dd971cfa8
2026-DEV2-017 <>
2026-01-19 20:26:10 +0100

    add five hours row to UI

--- BerlinClock/BerlinClock/Views/BerlinClockView.swift
+++ BerlinClock/BerlinClock/Views/BerlinClockView.swift
@@ -9,6 +9,14 @@ struct BerlinClockView: View {
             Circle()
                 .fill(model.seconds == "Y" ? Color.yellow : .clear)
                 .strokeBorder(lineWidth: 2)
+            
+            HStack {
+                ForEach(Array(model.fiveHours.enumerated()), id: \.offset) { _, character in
+                    Rectangle()
+                        .fill(character == "R" ? Color.red : .clear)
+                        .strokeBorder(lineWidth: 2)
+                }
+            }
         }
         .padding()
         .onAppear {
